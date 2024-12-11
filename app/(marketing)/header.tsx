ee15418328f3696b32fa44375fd7d65892fb1884
kostenkovitalina <152108911+kostenkovitalina@users.noreply.github.com>
2024-12-11 19:31:30 +0300

    Marketing Skeleton

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div>
+            <header className='h-20 w-full border-b-2 border-slate-200 px-4'>
+                Header
+            </header>
+        </div>
+    );
+};
+
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,7 @@
-import {Button} from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div className='text-green-500 font-bold'>
-      <Button size='lg'>Hello</Button>
+      Marketing page
     </div>
   );
 }
