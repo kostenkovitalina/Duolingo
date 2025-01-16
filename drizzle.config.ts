3d44ec94ca15f317c4c1760326346f3d123977ad
kostenkovitalina <152108911+kostenkovitalina@users.noreply.github.com>
2025-01-17 00:38:16 +0300

    Drizzle & Neon

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./db/schema.ts",
+    dbCredentials: {
+        host: "ep-withered-silence-a81229dy.eastus2.azure.neon.tech",
+        port: 5432,
+        user: "lingo_owner",
+        password: "yh7mUS3gMfTi",
+        database: "lingo",
+        ssl: true,
+    },
+});
