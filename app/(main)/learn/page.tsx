6ae3d65f76dbb0fcfb30b55cddaadd36b9c744fa
kostenkovitalina <152108911+kostenkovitalina@users.noreply.github.com>
2025-01-16 19:35:07 +0300

    Learn Page Wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {StickyWrapper} from "@/components/sticky-wrapper";
+import {FeedWrapper} from "@/components/feed-wrapper";
+import {Header} from "@/app/(main)/learn/header";
+import {UserProgress} from "@/components/user-progress";
 
 const LearnPage = () => {
     return (
-        <div>
-            LearnPage
+        <div className='flex flex-row-reverse gap-[48px] px-6'>
+            <StickyWrapper>
+                <UserProgress
+                    activeCourses={{title: 'Spanish', imageSrc: '/flags/es.svg'}}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title='Spanish'/>
+            </FeedWrapper>
         </div>
     );
 };
